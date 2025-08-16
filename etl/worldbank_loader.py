21f8e9618a8b79a490bf1be535cac2c02bbf1afd
Tobi Joshua <jpaullasisi@gmail.com>
2025-08-16 21:35:43 +0100

    engine created

--- etl/worldbank_loader.py
+++ etl/worldbank_loader.py
@@ -12,3 +12,6 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
